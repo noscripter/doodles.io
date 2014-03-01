d19a02caf02ded3495cca514b7d910a978280168
Joshua Croad <joshcroad@gmail.com>
2014-03-01 23:09:38 +0000

    Closed #26. Scrapbook is now implemented

--- public/js/views/scrapbook.js
+++ public/js/views/scrapbook.js
@@ -5,7 +5,7 @@ var Scrapbook = (function () {
   return {
 
     init: function () {
-      deleteButtons = document.getElementsByClassName('delete_button');
+      deleteButtons = document.getElementsByClassName('trash');
 
       this.bindEvents();
     },
@@ -17,17 +17,18 @@ var Scrapbook = (function () {
     },
 
     deleteClickHandler: function (e) {
+      var li = e.target.parentNode.parentNode.parentNode.parentNode;
       Utils.ajax({
-        url: '/' + e.target.parentNode.dataset.slug,
+        url: '/' + li.dataset.slug,
         method: 'DELETE',
         data: {
-          slug: e.target.parentNode.dataset.slug
+          slug: li.dataset.slug
         }
       }, function (res) {
         if (!res.success) {
           Utils.message(res.error, 'error');
         } else {
-          e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+          li.parentNode.removeChild(li);
           Utils.message('Doodle deleted successfully.', 'success');
         }
       });
